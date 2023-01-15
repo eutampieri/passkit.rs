0acbb8f56f7eb9859117f3266f2b2bdc563ae218
Eugenio Tampieri <eugenio@eutampieri.eu>
2023-01-15 13:32:32 +0100

    mmm

--- src/signature.rs
+++ src/signature.rs
@@ -10,11 +10,11 @@ fn sign(key: rsa::RsaPrivateKey, to_sign: &[u8]) -> Vec<u8> {
 }
 
 fn loremipsum() -> Result<(), ()> {
-    let certificate_bytes =
+    /*let certificate_bytes =
         include_bytes!("/Users/eugeniotampieri/Downloads/Cinema Pedagna - biglietti.p12");
     let document = dbg!(der::SecretDocument::try_from(certificate_bytes.as_slice()));
     let document = document.unwrap();
-    //rsa::RsaPrivateKey::from
+    //rsa::RsaPrivateKey::from*/
     Ok(())
 }
 
